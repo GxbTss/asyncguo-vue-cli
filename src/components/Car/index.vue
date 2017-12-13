86903b599326540fe52317f6d7ce0dbb17d8ff5b
guoxiaobo <xiaobo.guo@kongapi.com>
2017-12-13 17:57:42 +0800

    asyncguo-vue-cli

---
+++ src/components/Car/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="car">
+    <pagination :current-page="currentPage" :total-page="totalPage" @pageChange="getPost"></pagination>
+  </div>
+</template>
+
+<script>
+import Pagination from '@/base/Pagination';
+export default {
+  components: {
+    Pagination
+  },
+  data () {
+    return {
+      currentPage: 1,
+      totalPage: 121
+    }
+  },
+  methods: {
+    getPost (page) {
+      this.currentPage = page
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
